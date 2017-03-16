412b01b5e5444711626bf5f92a1c4795bf8199b2
Vasil Smolyanski <vasil.smolyanski@icb.bg>
2017-03-16 12:14:35 +0200

    Add sorting, paging and grouping

--- ContosoUniversity/src/ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/src/ContosoUniversity/Controllers/HomeController.cs
@@ -3,33 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
 
 namespace ContosoUniversity.Controllers
 {
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		private readonly SchoolContext _context;
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+		public HomeController(SchoolContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-            return View();
-        }
+		public async Task<ActionResult> About()
+		{
+			IQueryable<EnrollmentDateGroup> data =
+				from student in _context.Students
+				group student by student.EnrollmentDate into dateGroup
+				select new EnrollmentDateGroup()
+				{
+					EnrollmentDate = dateGroup.Key,
+					StudentCount = dateGroup.Count()
+				};
+			return View(await data.AsNoTracking().ToListAsync());
+		}
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
 
-            return View();
-        }
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
 
-        public IActionResult Error()
-        {
-            return View();
-        }
-    }
+			return View();
+		}
+
+		public IActionResult Error()
+		{
+			return View();
+		}
+	}
 }
